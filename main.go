e59facb7c0b14e8c07786a5dc61d26e1ee4f56a4
horechek <horechek@gmail.com>
2013-12-11 01:59:36 +0200

    added config

--- main.go
+++ main.go
@@ -1,27 +1,41 @@
 package main
 
 import (
+    "github.com/kylelemons/go-gypsy/yaml"
     "github.com/gorilla/websocket"
     "net/http"
     "log"
-    "flag"
     zmq "github.com/pebbe/zmq3"
 )
 
-var addr = flag.String("addr", ":8080", "http service address")
+var wsaddr = ":8080"
+var zmqaddr = "tcp://localhost:5563"
+var zmqsubject = "message"
 var responder *(zmq.Socket)
 
 func main() {
-    flag.Parse()
+    // parse config file
+    file := "config.yaml"
+    config, err := yaml.ReadFile(file)
+    if err != nil {
+        log.Fatal("Error load config", err)
+    }
 
+    wsaddr, _ = config.Get("ws.address")
+    zmqaddr, _ = config.Get("zmq.address")
+    zmqsubject, _ = config.Get("zmq.subject")
+
+    log.Println(wsaddr)
+
+    // connect to ZMQ broker
     responder, _ = zmq.NewSocket(zmq.SUB)
-    defer socket.Close()
-    responder.Connect("tcp://localhost:5563")
-    responder.SetSubscribe("message")
+    defer responder.Close()
+    responder.Connect(zmqaddr)
+    responder.SetSubscribe(zmqsubject)
 
-
+    // start handle ws
     http.HandleFunc("/ws", handler)
-    err := http.ListenAndServe(*addr, nil)
+    err = http.ListenAndServe(wsaddr, nil)
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
     }
@@ -33,16 +47,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
         http.Error(w, "Not a websocket handshake", 400)
         return
     } else if err != nil {
-
         log.Println(err)
         return
     }
 
     log.Println("Start...")
     for {
+        // gets message from ZMQ broker
         msg, _ := responder.RecvMessage(0)
         log.Println("Received ", msg)    
-        //websockets
+        // websockets
         if err := conn.WriteMessage(websocket.TextMessage, []byte(msg[1])); err != nil {
             log.Println(err)
             return
